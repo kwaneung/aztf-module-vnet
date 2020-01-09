0b217a667b35ada343b1a1ff54a8fb818c696387
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-09 16:42:54 +0900

    Update create_vnet.tf

--- create_vnet.tf
+++ create_vnet.tf
@@ -1,5 +1,7 @@
 resource "azurerm_virtual_network" "example" {
-  name                = var.vnet_name
+  count               = var.vnet_num
+  
+  name           			= var.vnet_num == 1 ? local.vm_name: format("%s-%02d", local.vnet_name, count.index + 1)
   location            = var.location
   resource_group_name = var.rg_name
   address_space       = var.addr_space
