73b850d33bab7ddc65743223387e285e50a9f0ef
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-09 17:07:57 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "prefix" {
 }
 
 variable "vnet_name" {
-  description = "vnet name"        
+  description = "Vnet name"        
 }
 
 variable "vnet_num" {
@@ -11,13 +11,21 @@ variable "vnet_num" {
 }
 
 variable "addr_space" {
-  description = "vnet address space"        
+  description = "Vnet address space"        
 }
 
 variable "location" {
-  description = "location"        
+  description = "Location"        
 }
 
 variable "rg_name" {
-  description = "resource_group_name"        
+  description = "Resource_group_name"        
 }
+
+variable "tag_key" {
+  description = "Key name of tag"        
+}
+
+variable "tag_value" {
+  description = "Value name of tag"        
+  }
