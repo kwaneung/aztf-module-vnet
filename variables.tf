563adbeb4e5a7507364ba93d7dcf4129bdd5f3a8
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-09 15:45:40 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  description = "Prefix for the workspace"
+}
+
+variable "vnet_name" {
+  description = "vnet name"        
+}
+
+variable "addr_space" {
+  description = "vnet address space"        
+}
