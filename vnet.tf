2cb50ca406c120c759ceaf8524dcfedb3f1a1baf
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 11:26:04 +0900

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "example_vnet" {
   count               = var.vnet_num
   
   name           			= var.vnet_num == 1 ?  format("%s_%s_%02d", "vnet", var.vnet_name): format("%s_%s_%02d", "vnet", var.vnet_name, count.index + 1)
