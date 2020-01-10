04dc40a7abaf365174b7111d9335646d4a942018
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 13:43:32 +0900

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -3,6 +3,5 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "${var.location}"
   address_space       = ["${var.address_space}"]
   resource_group_name = "${azurerm_resource_group.vnet.name}"
-  dns_servers         = "${var.dns_servers}"
   tags                = "${var.tags}"
 }
