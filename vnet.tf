087801be115f9e1870d45ec21827e8afd9521c2b
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-10 11:30:33 +0900

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "example_vnet" {
   count               = var.vnet_num
   
-  name           			= var.vnet_num == 1 ?  format("%s_%s_%02d", "vnet", var.vnet_name): format("%s_%s_%02d", "vnet", var.vnet_name, count.index + 1)
+  name           			= var.vnet_num == 1 ?  format("%s_%s", "vnet", var.vnet_name): format("%s_%s_%02d", "vnet", var.vnet_name, count.index + 1)
   location            = var.location
   resource_group_name = var.rg_name
   address_space       = var.addr_space
